af3e627c3b312d1f124b7bb70b099611b729e8c5
Vineet-Fullstack <vineet.fullstack.developer@gmail.com>
2025-02-04 13:10:32 +0530

    Update democli.rb

--- Formula/democli.rb
+++ Formula/democli.rb
@@ -1,17 +1,19 @@
 class Democli < Formula
-  desc "A description of your demoCLI tool"
-  homepage "https://github.com/Vineet-Fullstack/demoCLI"
-  url "https://github.com/Vineet-Fullstack/demoCLI/releases/download/v1.0.23/osx-x64.zip"
-  sha256 "5fb5a85545c98f488b970630d2c24e675be0ee19c8662adc189645ca680042e9"
-  version "v1.0.23"
+    desc "A description of your demoCLI tool"
+    homepage "https://github.com/Vineet-Fullstack/demoCLI"
+    url "https://github.com/Vineet-Fullstack/demoCLI/releases/download/v1.0.23/osx-x64.zip"
+    sha256 "9345ae6e93011a62f9b4443ba8e896ed5a5a25a37f437e0349042928c94f3bb5"
+    version "v1.0.23"
 
-  def install
-    # Assuming the zip contains a macOS binary named `demoCLI`
-    bin.install "demoCLI"
-  end
+    def install
+      libexec.install Dir["*"]  # ✅ Copy everything into libexec
 
-  test do
-    # Add a simple test to verify the installation
-    system "#{bin}/demoCLI", "--version"
+      (bin/"demoCLI").write <<~EOS
+        #!/bin/bash
+        DOTNET_ROOT=$(brew --prefix dotnet-runtime)
+        exec "$DOTNET_ROOT/dotnet" "#{libexec}/demoCLI.dll" "$@"
+      EOS
+
+      chmod "+x", bin/"demoCLI"
+    end
   end
-end
