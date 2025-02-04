b13c5be2e3aad309bdf8bfbb000e24ec42beda2a
Vineet-Fullstack <vineet.fullstack.developer@gmail.com>
2025-02-04 12:42:50 +0530

    Update democli.rb

--- Formula/democli.rb
+++ Formula/democli.rb
@@ -1,20 +1,11 @@
 class Democli < Formula
-    desc "A description of your demoCLI tool"
-    homepage "https://github.com/Vineet-Fullstack/demoCLI"
-    url "https://github.com/Vineet-Fullstack/demoCLI/releases/download/v1.0.21/osx-x64.zip"
-    sha256 "9345ae6e93011a62f9b4443ba8e896ed5a5a25a37f437e0349042928c94f3bb5"
-    version "v1.0.21"
-  
-    def install
-      libexec.install Dir["*"]  # ✅ Copy everything into libexec
-  
-      (bin/"demoCLI").write <<~EOS
-        #!/bin/bash
-        DOTNET_ROOT=$(brew --prefix dotnet-runtime)
-        exec "$DOTNET_ROOT/dotnet" "#{libexec}/demoCLI.dll" "$@"
-      EOS
-  
-      chmod "+x", bin/"demoCLI"
-    end
+  desc "A description of your demoCLI tool"
+  homepage "https://github.com/Vineet-Fullstack/demoCLI"
+  url "https://github.com/Vineet-Fullstack/demoCLI/releases/download/v1.0.21/osx-x64.zip"
+  sha256 "9345ae6e93011a62f9b4443ba8e896ed5a5a25a37f437e0349042928c94f3bb5"
+  version "v1.0.21"
+
+  def install
+    bin.install "demoCLI"  # Directly install the self-contained binary
   end
-  
+end
