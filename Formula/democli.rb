550698886cec2dc0c79852b60a22ecfbe70a2883
Vineet-Fullstack <vineet.fullstack.developer@gmail.com>
2025-02-19 22:42:35 +0530

    Update democli.rb

--- Formula/democli.rb
+++ Formula/democli.rb
@@ -1,7 +1,7 @@
 class Democli < Formula
   desc "A description of your demoCLI tool"
   homepage "https://github.com/Vineet-Fullstack/demoCLI"
-  url "https://github.com/Vineet-Fullstack/demoCLI/releases/download/v1.0.23/osx-x64.zip"
+  url "https://github.com/Vineet-Fullstack/demoCLI/releases/download/v1.0.26/osx-x64.zip"
   sha256 "5fb5a85545c98f488b970630d2c24e675be0ee19c8662adc189645ca680042e9"
   version "1.0.23"
 
